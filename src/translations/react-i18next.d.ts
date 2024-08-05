98f1506a0cd189ddfbab734295a292d57334bcd5
Minh Le <lqminh2207@gmail.com>
2024-08-05 13:46:15 +0700

    remove unused file

--- src/translations/react-i18next.d.ts
+++
@@ -1,8 +0,0 @@
-import type { resources, defaultNS } from './i18n';
-
-declare module 'react-i18next' {
-  interface CustomTypeOptions {
-    defaultNS: typeof defaultNS;
-    resources: (typeof resources)['en'];
-  }
-}
